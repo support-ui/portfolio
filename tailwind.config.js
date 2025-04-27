ba550eb4c9dde88c6e13328c0b3e27e50a1aa313
support-ui <luis.dcr25@gmail.com>
2025-04-26 22:30:14 -0500

    Quedó el diseño de Skills

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./src/**/*.{astro,html,js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Poppins', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
